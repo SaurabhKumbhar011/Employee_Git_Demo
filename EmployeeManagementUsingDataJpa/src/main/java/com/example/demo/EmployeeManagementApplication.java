47ae18b430d34441dc3803d54757ab621e385660
SaurabhKumbhar011 <saurabhkumbhar011@gmail.com>
2026-03-13 14:40:48 +0530

    second commit from sts

--- EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -8,6 +8,7 @@ public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("First Line in sts after git");
+		System.out.println("Second Line in sts after git");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
